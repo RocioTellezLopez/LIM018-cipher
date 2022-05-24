b79c814a5df3a15485a6b368d3ce15ef6ac8d04a
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-05-24 12:06:42 -0500

    Avance de html y css

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
 const cipher = {
-  // ...
+
+  /*offset número de posiciones a la derecha, string el mensaje (texto) ha cifrar.*/
+  cipher.encode(offset, string)
+
+
+
+  /*offset número de posiciones a la izquierda , string el mensaje (texto) ha descifrar.*/
+  cipher.decode(offset, string)
+
+
+
+
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
 
+cipher.encode()
+
+
+cipher.decode()
+
+
 console.log(cipher);
