e932fcdc46c7682defc4a2122bb0189ecd235c1e
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-05-30 09:22:27 -0500

    Funcion decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,38 @@
 const cipher = {
    // ...
    encode: (desplazamiento,mensaje) =>{
-    let repeticion = mensaje.length
+    let iteracion = mensaje.length
     let mostrarMensaje = ''
 
-    for (let i=0 ; i<repeticion ; i++) {
+    for (let i=0 ; i<iteracion ; i++) {
       let posicionAcsii =(mensaje.charCodeAt(i) - 65 + desplazamiento) % 26 + 65
       let mensajeCifrado = String.fromCharCode(posicionAcsii)
       mostrarMensaje = mostrarMensaje + mensajeCifrado
       
     }
-    console.log(mostrarMensaje)
+    //console.log(mostrarMensaje)
     return mostrarMensaje
 
-  }
+  },
 
-  decode: (desplazamiento,mensaje) => {}
+  decode: (desplazamiento,mensaje) =>{
+    let iteracion = mensaje.length
+    let mostrarMensaje = ''
+
+    for (let i=0; i<iteracion; i++) {
+      let posicionAcsii = (mensaje.charCodeAt(i) + 65 - desplazamiento) % 26 + 65
+      let mensajeDecifrado = String.fromCharCode(posicionAcsii)
+      mostrarMensaje = mostrarMensaje + mensajeDecifrado 
+    }
+    //console.log(mostrarMensaje)
+    return mostrarMensaje
+  }
 
 }
  
 
-cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+//cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+//cipher.decode(33,'HIJKLMNOPQRSTUVWXYZABCDEFG')
 
 
 export default cipher;
